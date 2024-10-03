89b0164370eb8a3c4281142896b5a85446272b88
Jorge Vargas <luisvardez@gmail.com>
2024-10-02 22:06:53 -0600

    Termino preguntas frecuentes y funcionalidad partidos js

--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,21 @@
-const steps = document.getElementsByClassName("content-steps");
+function changeClass(element){
+    element.nextElementSibling.classList.toggle("d-none");
+
+    if(!element.nextElementSibling.classList.contains("d-none")){
+       element.querySelector("span").innerHTML = "-";
+    }else{
+        element.querySelector("span").innerHTML = "+";
+    }
+}
+
+
+function selectPartido(element){
+    let partidos = document.querySelectorAll(".partido");
+
+    for (var i = 0; i < partidos.length; i++) {
+        partidos[i].classList.remove('active');
+     }
+
+    element.classList.toggle("active");
+}
 
